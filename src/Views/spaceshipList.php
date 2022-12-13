0fd2a6609b402abddc2a2315549b24eeee1031c1
LolaGM <canal030w@gmail.com>
2022-12-13 10:46:27 +0100

    muted code eliminated

--- src/Views/spaceshipList.php
+++ src/Views/spaceshipList.php
@@ -13,14 +13,7 @@
 
             <?php
 
-                foreach($data["spaceship"] as $spaceship){
-                    /* echo "{$spaceship->getId()}";
-                    echo "{$spaceship->getSpaceship()}";
-                    echo "{$spaceship->getCommander()}";
-                    echo "{$spaceship->getMessage()}";
-                    echo "{$spaceship->getSector()}";
-                    echo "{$spaceship->getDate()}";*/
-                    
+                foreach($data["spaceship"] as $spaceship){                          
                     
                     echo "
                     
